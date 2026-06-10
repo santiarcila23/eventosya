09e46a97da2dea880ea3ad0cb77af9402a15232f
santiarcila23 <santiarcila270@gmail.com>
2026-06-09 21:47:20 -0500

    Elimina Dockerfile usa Nixpacks

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /var/www/html
-/usr/local/bin/php artisan config:clear || true
-/usr/local/bin/php artisan migrate --force || true
-echo "Starting server on port ${PORT:-8000}"
-exec /usr/local/bin/php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
